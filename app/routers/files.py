e9dc13f87534c83b8d7398d5c797689ed785a0ff
mdshamoon <mohdshamoon841@gmail.com>
2026-01-17 12:45:51 +0530

    pdf response

--- app/routers/files.py
+++ app/routers/files.py
@@ -106,7 +106,7 @@ async def process_file_and_callback(request_data: dict):
     
     # Send result to Glific
     result_data = {
-        "filesearch_response": openai_response,
+        "pdf_response": openai_response,
         "query": prompt,
         "file_url": file_url,
         "processed_at": datetime.now().isoformat(),
